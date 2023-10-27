7dd2fde16cc35c3dc9825cda30262cfb55e7293b
yonessgt <youness.merzak123@gmail.com>
2023-10-27 23:04:21 +0000

    adding the api/v1/stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,29 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """ returning a json with status : ok """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('stats', methods=['GET'])
+def stats():
+    """ retrieves the number of each objects by type """
+    return jsonify({
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            })
